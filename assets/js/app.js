a1b0c3d225c56df349963d861cbf846e30bbbdf7
giacomomarongiu <giacomomarongiu1993@gmail.com>
2024-03-12 14:31:26 +0100

    add images + start using VUE + can see the first movies in my DOM

--- assets/js/app.js
+++ assets/js/app.js
@@ -9,29 +9,34 @@ createApp({
         title: "Il Signore degli anelli",
         category: "fantasy",
         length: 180,
+        img: "./assets/img/01.jpg",
         description: "lorem ipsum lorem ipsumlorem ipsuml lor em ipsumorem ipsumlorem ipsum"
       },
       {
         title: "Il buono, il brutto e il cattivo",
         category: "fatasy",
         length: 121,
+        img: "./assets/img/02.jpeg",
         description: "lorem ipsum lorem ipsumlorem ipsuml lor em ipsumorem ipsumlorem ipsum"
       },
       {
         title: "Pulp Fiction",
         category: "fatasy",
         length: 123,
+        img: "./assets/img/03.jpeg",
         description: "lorem ipsum lorem ipsumlorem ipsuml lor em ipsumorem ipsumlorem ipsum"
       }, {
         title: "Star Wars - Rogue One",
         category: "fatasy",
         length: 109,
+        img: "./assets/img/04.jpg",
         description: "lorem ipsum lorem ipsumlorem ipsuml lor em ipsumorem ipsumlorem ipsum"
       },
       {
         title: "Le iene",
         category: "fatasy",
         length: 115,
+        img: "./assets/img/05.jpg",
         description: "lorem ipsum lorem ipsumlorem ipsuml lor em ipsumorem ipsumlorem ipsum"
       }
       ]
